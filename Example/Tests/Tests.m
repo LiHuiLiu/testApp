d956a62280a306e77ed9f430ead01c4cb268a8da
llh123 <1622863364@qq.com>
2019-10-10 10:00:42 +0800

    Initial commit

---
+++ Example/Tests/Tests.m
@@ -0,0 +1,35 @@
+//
+//  test1Tests.m
+//  test1Tests
+//
+//  Created by 1622863364@qq.com on 10/10/2019.
+//  Copyright (c) 2019 1622863364@qq.com. All rights reserved.
+//
+
+@import XCTest;
+
+@interface Tests : XCTestCase
+
+@end
+
+@implementation Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
+
